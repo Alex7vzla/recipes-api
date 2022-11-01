d3d46e34acafc79c82cda4d4e4e7520b599a5686
unknown <alejandro117925@gmail.com>
2022-11-01 15:16:46 -0400

    Details router

--- src/categories/categories.routes.js
+++ src/categories/categories.routes.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
+const passport = require('passport');
+const adminValidate = require('../middlewares/role.middleware');
 const categoriesServices = require('./categories.services');
-
+require('../middlewares/auth.middleware')(passport)
 
 router.route('/')
     .get(categoriesServices.getAllCategories)
     .post(categoriesServices.postCategory)
 
 router.route('/:id')
-    .get(categoriesServices.getCategorieById)
-    .delete(categoriesServices.deleteCategory)
+    .get(
+        passport.authenticate('jwt', {session: false}),
+        adminValidate,
+        categoriesServices.getCategorieById
+        )
+    .delete(
+        passport.authenticate('jwt', {session: false}),
+        adminValidate,
+        categoriesServices.deleteCategory
+        )
+
+module.exports = router;
--- src/users/users.router.js
+++ src/users/users.router.js
@@ -5,23 +5,20 @@ const userServices = require('./users.services')
 
 require('../middlewares/auth.middleware')(passport)
 
-
-//? rutas raiz
+// rutas raiz
 
 router.get('/', userServices.getAllUsers)
 
 //TODO el registerUser ira en la ruta /auth/register
 
-//! router.route('/').get( userServices.getAllUsers)
+// rutas dinamicas por ID /users/:id
 
-//? rutas dinamicas por ID /users/:id
+// router.get('/:id')
+// router.patch('/:id')
+// router.put('/:id')
+// router.delete('/:id')
 
-//! router.get('/:id')
-//! router.patch('/:id')
-//! router.put('/:id')
-//! router.delete('/:id')
-
-//? Ruta de informacion propia del usuario loggeado
+// Ruta de informacion propia del usuario loggeado
 router.route('/me')
     .get(
         passport.authenticate('jwt', {session: false}),
@@ -35,7 +32,7 @@ router.route('/me')
         userServices.deleteMyUser
     )
 
-//? /api/v1/users/:id
+// /api/v1/users/:id
 router.route('/:id')
     .get(userServices.getUserById)
     .patch(
@@ -48,9 +45,5 @@ router.route('/:id')
         adminValidate,
         userServices.deleteUser
     )
-
-
-
-
 
 module.exports = router
